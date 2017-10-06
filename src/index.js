55f1b2281669ebba31b9c14b270b8035f23ed680
Akash <akash.gutha@outlook.com>
2017-10-06 10:32:10 +0530

    Integrating browser router (#5)
    * prettified App file
    
    * Integrating browser router into application

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import App from "./App";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
